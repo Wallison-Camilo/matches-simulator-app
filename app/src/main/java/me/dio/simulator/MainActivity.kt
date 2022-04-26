57397051e3cf84836a5f01da64942edac929e3ba
Wallison-Camilo <101194335+Wallison-Camilo@users.noreply.github.com>
2022-04-26 11:55:05 -0300

    Revert "Desenvolvimento dos layouts de listagens e detalhes de partidas"

--- app/src/main/java/me/dio/simulator/MainActivity.kt
+++ app/src/main/java/me/dio/simulator/MainActivity.kt
@@ -2,17 +2,10 @@ package me.dio.simulator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import me.dio.simulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        //val TVHello = findViewById<TextView>(R.id.TVHello)
+        setContentView(R.layout.activity_main)
     }
 }
